0c911eb55cf87aa1cbf383fee480f6e6fac0d0c4
cookiemonster4 <cookiemonster444@protonmail.com>
2020-04-23 16:59:03 +0300

    refactor - change the binding according to codelabs

--- app/src/main/java/com/elyonut/wow/adapter/MapLayersAdapter.kt
+++ app/src/main/java/com/elyonut/wow/adapter/MapLayersAdapter.kt
@@ -8,6 +8,8 @@ import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.elyonut.wow.R
 import com.elyonut.wow.databinding.MapLayerItemBinding
@@ -16,78 +18,64 @@ import com.elyonut.wow.model.MapLayer
 
 class MapLayersAdapter(
     var context: Context,
-    var mapLayers: ArrayList<MapLayer>,
-    onClickHandler: OnClickInterface,
     private val clickListener: MapLayerClickListener
-) : RecyclerView.Adapter<MapLayersAdapter.MapLayersViewHolder>() {
+) : ListAdapter<MapLayer, MapLayersAdapter.MapLayersViewHolder>(MapLayerDiffCallback()) {
 
-    var onClickInterface: OnClickInterface = onClickHandler
     var selectedItemIndex: Int = 0
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MapLayersViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val view = DataBindingUtil.inflate<MapLayerItemBinding>(
-            inflater,
-            R.layout.map_layer_item,
-            parent,
-            false
-        )
-        return MapLayersViewHolder(view)
-    }
-
-    override fun getItemCount(): Int {
-        return mapLayers.count()
+        return MapLayersViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: MapLayersViewHolder, position: Int) {
-        holder.view.mapLayer = mapLayers[position]
-        holder.view.listener = clickListener
+        val item = getItem(position)
+        holder.bind(item, clickListener, position, selectedItemIndex, context)
+    }
 
-//        holder.mapName.text = mapLayers[position].name
-//        holder.mapTypeImage.setImageResource(mapLayers[position].image)
-//
-//        if(selectedItemIndex == position){
-//            holder.frame.background = context.getDrawable(R.drawable.card_selected_edge)
-//        }
-//        else
-//        {
-//            holder.frame.background = context.getDrawable(R.drawable.card_edge)
-//        }
+    class MapLayersViewHolder private constructor(private val binding: MapLayerItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
+        fun bind(
+            item: MapLayer,
+            clickListener: MapLayerClickListener,
+            position: Int,
+            selectedItemIndex: Int,
+            context: Context
+        ) {
+            binding.mapLayer = item
+            binding.clickListener = clickListener
 
-        if(selectedItemIndex == position){
-            holder.view.mapItemFrame.background = context.getDrawable(R.drawable.card_selected_edge)
+            if (selectedItemIndex == position) {
+                binding.mapItemFrame.background = context.getDrawable(R.drawable.card_selected_edge)
+            } else {
+                binding.mapItemFrame.background = context.getDrawable(R.drawable.card_edge)
+            }
         }
-        else
-        {
-            holder.view.mapItemFrame.background = context.getDrawable(R.drawable.card_edge)
+
+        companion object {
+            fun from(parent: ViewGroup): MapLayersViewHolder {
+                val inflater = LayoutInflater.from(parent.context)
+                val binding = MapLayerItemBinding.inflate(inflater, parent, false)
+                return MapLayersViewHolder(binding)
+            }
         }
     }
 
-    class MapLayersViewHolder(var view: MapLayerItemBinding) : RecyclerView.ViewHolder(view.root)
+    fun setClickedPosition(position: Int) {
+        selectedItemIndex = position
+    }
 
     class MapLayerClickListener(val clickListener: (mapLayerId: String) -> Unit) {
         fun onClick(mapLayer: MapLayer) = clickListener(mapLayer.id)
     }
+}
 
-//    override fun onMapLayerClicked(view: View) {
-////        selectedItemIndex =
-////        onClickInterface.setClick(view, this.adapterPosition)
-////        notifyDataSetChanged()
-//    }
-//        val mapTypeImage: ImageView = view.findViewById(R.id.map_type_image)
-//        val mapName: TextView = view.findViewById(R.id.map_type_name)
-//        val frame: FrameLayout = view.findViewById(R.id.map_item_frame)
-//
-//        init{
-//            mapTypeImage.setOnClickListener(this)
-//            mapName.setOnClickListener(this)
-//            frame.setOnClickListener(this)
-//        }
-//
-//        override fun onClick(p0: View?) {
-////            selectedItemIndex = this.adapterPosition
-////            onClickInterface.setClick(p0!!, this.adapterPosition)
-////            notifyDataSetChanged()
-//        }
+class MapLayerDiffCallback : DiffUtil.ItemCallback<MapLayer>() {
+    override fun areItemsTheSame(oldItem: MapLayer, newItem: MapLayer): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: MapLayer, newItem: MapLayer): Boolean {
+        return oldItem == newItem
+    }
 }
--- app/src/main/java/com/elyonut/wow/view/MapLayerClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.elyonut.wow.view
-
-import android.view.View
-
-//interface MapLayerClickListener {
-//    fun onMapLayerClicked(view: View)
-//}
